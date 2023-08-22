33b149bc372e8064604f31cf81515aca5cc79415
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2023-08-23 00:15:33 +0530

    C++ Dummy Code

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world"<<endl;
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    cout<<"Your Number is "<<n<<endl;
+}
