8cd57fd0d388b994d3072bd1001235d0516e0877
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2023-09-19 04:05:21 +0530

    Solving some questions

---
+++ question/nto1-number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int n;
+    cout<<"Enter your number ";
+    cin>>n;
+    for(int i=n; i>=1; i--)
+    {
+        cout<<i<<endl;
+    }
+}
